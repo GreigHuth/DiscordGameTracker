82f8f746546043127fadab12af6cbd08ebed6030
Greigashaska <greighuth@hotmail.co.uk>
2019-02-28 21:10:28 +0000

    implemented logic to keep track of games being played

--- timebot.py
+++ timebot.py
@@ -1,29 +1,64 @@
 #bot to keep track of how many and how often games are played on discord
-
 import discord
+import logging
+import time
+from user import * 
 
+logging.basicConfig(level=logging.INFO)#logs errors and debug info
+
+
+#global variables-------------------------------------------
+current_playing = []
+
+
+# token for test server
 TOKEN = "NTM5MTQwNTExMjM2MzU4MTQ0.Dy-Bdg.ZEiEWfMm_ji1I0qdpbDqJDAJSog"
+
+
 client = discord.Client()
 
 @client.event
 async def on_ready():
 	print("The bot is ready!")
-	await client.change_presence(game=discord.Game(name="Making a bot"))
+	await client.change_presence(game=discord.Game(name="tick, tock"))
 
 @client.event
 async def on_message(message):
-	if message.author == client.user:
-		return
-	if message.content == "!time":
-		game = message.author.game
-		print(type(game))
-		if game is None:
-			await client.send_message(message.channel, "no game")
-		await client.send_message(message.channel, hash(game))
+	pass
+
 
 @client.event
 async def on_member_update(before, after):
-	print (before.game)
-	print (after.game)
+	if before.bot == True: # if the user is a bot ignore it
+		return
+
+	if before.game == None: # user has just started playing a game
+		print ("UPDATE: user %s has started playing %s." % (before.name, after.game.name))
+
+		id = before.id
+		game = after.game.name 
+		start = time.time()
+		end = None
+
+		user= User(id, game, start, end) #creates new user
+		current_playing.append(user) # adds new user to list of people currently playing games
+	
+
+	elif after.game ==  None: # user has just stopped playing a game
+		print ("UPDATE: user %s has stopped playing %s." %(after.name, before.game.name ))
+
+		end = time.time() 
+		for user in current_playing:
+			# find current user in list of users playing games
+			if after.id == user.id and before.game.name == user.game: 
+				user.end = end # time user stopped playing the game
+				time_played = user.end - user.start # total time the game was played 
+				current_playing[user].end = end
+				print ("UPDATE: user %s played %s for %d seconds." % (user.name, before.game.name, time_played)) 
+				current_playing.remove(user)
+
+
+
+	
 
 client.run(TOKEN)
---
+++ user.py
@@ -0,0 +1,7 @@
+class User:
+	def __init__(self, id, game, start, end):
+		self.id = id
+		self.game = game
+		self.start = start
+		self.end = end
+	
