8c131ef47b8daafc51d62190c9fe0e1b875bfdd3
Greig Huth <greighuth@hotmail.co.uk>
2019-03-14 23:43:36 +0000

    fixed

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 # pm Gurgashaska#6315 or email greighuth@hotmail.com if there are any issues
 
 # Paste token in here
-TOKEN = "NTUyNTAxOTQ0MTQ1NDEyMDk2.D2DFew.euynASdk_zuFtidGDslCDrlx_Zk"
+TOKEN = ""
